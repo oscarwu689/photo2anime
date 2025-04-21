abe97c63121ebd8d43b0da6f7524ec5f5ea8956c
Oscar.Wu <zxxz8700@gmail.com>
2025-04-21 16:45:29 +0800

    fix: use PORT env variable for Render

--- app.py
+++ app.py
@@ -97,4 +97,5 @@ def stylize():
     return send_file(final_path, mimetype="image/png")
 
 if __name__ == "__main__":
-     app.run(debug=True, host="0.0.0.0", port=5000)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(debug=False, host="0.0.0.0", port=port)
